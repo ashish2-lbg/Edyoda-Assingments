2c2efbb2e5f0fe59d64c0051fc2680867a46b96e
ashish730750 <111068948+ashish730750@users.noreply.github.com>
2022-08-21 21:28:31 +0530

    Add files via upload

---
+++ Assignment2_Ashish.py.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+#Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+#Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+tupList =[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
+print("List of Tuple before sorting : " + str(tupList))
+
+listLen = len(tupList); 
+for i in range(0, listLen):
+    for j in range(0, listLen - i - 1):
+        if(tupList[j][-1] > tupList[j + 1][-1]):
+            swap = tupList[j]
+            tupList[j] = tupList[j + 1]
+            tupList[j + 1] = swap
+
+print("List of Tuple after sorting : " + str(tupList))
+
+
+# In[2]:
+
+
+#Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+#Sample Output : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
+
+a = dict()
+for i in range(97,123):
+    a[chr(i)] = str(i)
+print(a)
+
+
+# In[ ]:
+
+
+
+
